c22d4ee29944c6cd38e2f01d3a7a15d3c0abf357
Justin "J.R." Hill <justin@so.dang.cool>
2025-05-23 23:38:16 -0700

    true and false

--- build.sh
+++ build.sh
@@ -4,20 +4,24 @@ this="$(basename "$0")"
 root="$(dirname "$0")"
 outbin="$root/out/bin"
 
-# TODO: handle more than one util
-util=wc
+utils="false true wc"
 
 for step in "$@"
 do
   case "$1" in
     help) echo "USAGE: $this [ARG]" ;;
 
-    build) mkdir -p "$outbin"; zig build-exe "$root/src/$util.zig" -femit-bin="$outbin/$util" ;;
+    build)
+      mkdir -p "$outbin"
+      for util in $utils;do
+        zig build-exe "$root/src/$util.zig" -femit-bin="$outbin/$util"
+      done
+      ;;
     clean) rm -r "$outbin" ;;
     e2e) "$root/tests/run" ;;
     fmt) zig fmt "$root/src" ;;
-    fuzz) zig test -ffuzz "$root/src/$util.zig" ;;
-    test) zig test "$root/src/$util.zig" ;;
+    fuzz) for util in $utils; do zig test -ffuzz "$root/src/$util.zig"; done ;;
+    test) for util in $utils; do zig test "$root/src/$util.zig"; done ;;
   esac
   shift
 done
